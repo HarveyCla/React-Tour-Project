6389cbccac889ef50fd1976a7207c0b97fe72331
HarveyCla <harvey@clarusway.com>
2022-03-19 05:30:11 +0300

    cards done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Header from './components/header/Header';
+import Card from './components/card/Card';
 
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Header/>
+        <Card/>
     </div>
   );
 }
